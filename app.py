6d408ecfe691e339c99db35b73f8142480a11dc1
alex-andersen <alex@Alexandres-MacBook-Pro-2.local>
2017-08-13 19:19:59 +0200

    Basic Logger

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 import logging
-import configparser
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+handler = logging.FileHandler("logfile.log")
+handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+
+logger.info("This is an info message in logfile")
 
 
-def main():
 
-    print('Hello World')
+def add(x, y):
+    '''Add Function'''
+    return x + y
 
+num1 = 10
+num2 = 5
 
-
-
-
-if __name__ == '__main__':
-    main()
+add_result = add(num1, num2)
+logger.info('Add: {} + {} = {}' .format(num1, num2, add_result))
